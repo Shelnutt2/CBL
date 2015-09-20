e2207f74cce49f3be5ce12fdb54c825b210a74fb
Seth Shelnutt <shelnutt2@gmail.com>
2015-09-20 13:05:33 -0400

    Intial commit of test launcher

---
+++ app/src/main/java/nu/shel/cbl/AppDetail.java
@@ -0,0 +1,12 @@
+package nu.shel.cbl;
+
+/**
+ * Created by seth on 9/20/15.
+ */
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
